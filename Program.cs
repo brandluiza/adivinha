c5da68c0f589f6c9240e40244e9e52cda9acf971
brandluiza <e107ti@etec.sp.gov.br>
2025-03-11 21:39:16 -0300

    finalizar programa

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.WriteLine(" O que é, O que é... ");Thread.Sleep(1000);
+Console.WriteLine(" que tem coroas e escamas mas não é peixe? ");
+Thread.Sleep(5000);
+Console.WriteLine(" O Abacaxi. ");
